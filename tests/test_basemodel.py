1d97a28f0b08cb138b01220cf23cb255c831e6f8
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-01-23 10:35:51 -0800

    update

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import unittest
-from models.base_model import
+from models.base_model import BaseModel
 
 class testBasemodel(unittest.TestCase):
 """checks every property of an instance"""
