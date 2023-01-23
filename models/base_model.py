da0eb3f61f8862d96e497663cc33ccf4c7f46a5a
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-01-23 09:31:34 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+"""
+Create class BaseModel that  defines all common
+attributes/methods for other classes
+"""
+class BaseModel:
+    def __init__(self):
+        """Initialize"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """returns string representation of an instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime """
+        self.save = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dictionary = self.__dict__.copy()
+        dictionary.update({"__class__": self.__class__.__name__})
+        dictionary.update({"created_at": self.created_at.isoformat()})
+        dictionary.update({"updated_at": self.updated_at.isoformat()})
+        return (dictionary)
