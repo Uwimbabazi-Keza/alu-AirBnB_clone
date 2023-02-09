fb434ee0b27d95844e27d85a2378f98652645122
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-09 12:48:52 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 """
 Create class BaseModel that  defines all common
 attributes/methods for other classes
 """
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs)>0:
+            for key, value in kwargs.items():
+                if key != "__class__" or (key == "created_at" or key == "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """returns string representation of an instance"""
