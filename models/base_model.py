8bf6de7405a7d9d36fb5f49546127004297a11af
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-09 15:06:28 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,16 +12,17 @@ class BaseModel:
         """Initialize"""
         if len(kwargs)>0:
             for key, value in kwargs.items():
-                if key != "__class__" or (key == "created_at" or key == "updated_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
                     setattr(self, key, value)
-                else:
+                
+                if (key == "created_at" or key == "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
+    
     def __str__(self):
         """returns string representation of an instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
