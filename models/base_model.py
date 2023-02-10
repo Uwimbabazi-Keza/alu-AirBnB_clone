9c02d931827020cc828b04fa86c14cc67b345cd2
Husnafazal <101583586+Husnafazal@users.noreply.github.com>
2023-02-10 19:36:57 +0300

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,16 @@ import models
 Create class BaseModel that  defines all common
 attributes/methods for other classes
 """
+
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize"""
-        if len(kwargs)>0:
+        if len(kwargs)> 0:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-                
+                    
                 if (key == "created_at" or key == "updated_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
@@ -22,17 +24,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-    
+            
     def __str__(self):
         """returns string representation of an instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime """
+        """updates the public instance attribute 
+        updated_at with the current datetime """
         self.save = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
         dictionary = self.__dict__.copy()
         dictionary.update({"__class__": self.__class__.__name__})
         dictionary.update({"created_at": self.created_at.isoformat()})
