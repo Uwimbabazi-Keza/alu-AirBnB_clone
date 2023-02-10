ca5706390cac0d41dcccdd00fc9ff9b6ddf4f561
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-10 02:55:33 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import json
-
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
@@ -18,14 +24,6 @@ class FileStorage:
             json.dump(json_objects, file)
 
     def reload(self):
-
-        from models.base_model import BaseModel
-        from models.amenity import Amenity
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
-        from models.user import User
         try:
             with open(self.__file_path, "r", encoding="UTF8") as file:
                 data = json.load(file)
