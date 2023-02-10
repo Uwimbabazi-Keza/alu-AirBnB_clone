63ffb4e98deac7704d621de26061f4531374548d
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-09 22:11:34 -0800

    update

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import unittest
+from models.state import State
+
+"""tests for class State"""
+
+class test_state(unittest.TestCase):
+    
+    def test_init(self):
+        """tests"""
+        state = State()
+        state.name = "Eastern Cape"
+        self.assertIsInstance(state, State)
+        self.assertEqual(state.name, "Eastern Cape")
+        self.assertEqual(str(type(state)), "<class 'models.state.State'>")
+        
+if __name__ == "__main__":
+    unittest.main()
