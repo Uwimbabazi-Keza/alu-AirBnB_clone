cf41e147923babbf3e8d6ebbe1e6bdcdb8c9f5ea
husnafazal <h.ahmed1@alustudent.com>
2023-05-02 16:35:07 +0300

    made some changes

--- models/base_model.py
+++ models/base_model.py
@@ -24,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
             
     def __str__(self):
         """returns string representation of an instance"""
@@ -32,7 +33,8 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute 
         updated_at with the current datetime """
-        self.save = datetime.now()
+        self.updated_at= datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all
