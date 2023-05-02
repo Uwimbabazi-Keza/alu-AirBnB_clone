ca97413ebb7846e948dfa78eab7ef4b75591f377
husnafazal <h.ahmed1@alustudent.com>
2023-05-02 16:23:05 +0300

    made some changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,64 @@
 #!/usr/bin/python3
+"""file storage"""
 import json
+import sys
 from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}
+
+
 class FileStorage:
-    __file_path = "file.json"
+    __file_path = "./file.json"
     __objects = {}
+    """instantiate file storage"""
+
+    def __init__(self):
+        pass
+
+    """return the dictionary"""
 
     def all(self):
         return self.__objects
 
+    """add new dictionary"""
+
     def new(self, obj):
-        key = obj.__class__.__name__+ "." + str(obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
+    """save __objects json in a file"""
+
     def save(self):
-        json_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        objs = {}
+        for key in self.__objects.keys():
+            objs[key] = self.__objects[key].to_dict()
         with open(self.__file_path, "w") as file:
-            json.dump(json_objects, file)
+            json.dump(objs, file)
+
+    """get dict from file"""
 
     def reload(self):
         try:
-            with open(self.__file_path, "r", encoding="UTF8") as file:
+            with open(self.__file_path, "r") as file:
                 data = json.load(file)
                 for key in data.keys():
-                    value = data[key]
-                    self.__objects[key] = eval(value["__class__"])(**value)
+                    cls = classes[data[key]["__class__"]]
+                    obj = cls(**data[key])
+                    self.new(obj)
+                return data
         except FileNotFoundError:
             pass
